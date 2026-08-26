68ddf30ba089c073fa0f1d25dbe5968b99717c91
novygh <32515025+novygh@users.noreply.github.com>
2026-08-26 20:23:28 +0200

    Remove one-shot repair services from runtime

--- custom_components/enea_rcem/__init__.py
+++ custom_components/enea_rcem/__init__.py
@@ -8,11 +8,8 @@ from homeassistant.core import HomeAssistant
 from .compensation import CompensationReconciler
 from .const import PLATFORMS
 from .deposit import DepositCoordinator
-from .repair import register_repair_service
 from .runtime import EneaRcemRuntime
 from .settled_daily import SettledDailyCoordinator
-from .shortterm_repair import register_shortterm_repair_service
-from .state_repair import register_state_repair_service
 from .statistics_alignment import StatisticsAligner
 
 
@@ -26,9 +23,6 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     entry.runtime_data = runtime
     runtime.statistics_aligner = statistics_aligner
 
-    register_repair_service(hass)
-    register_state_repair_service(hass)
-    register_shortterm_repair_service(hass)
     entry.async_on_unload(entry.add_update_listener(_async_update_listener))
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
 
